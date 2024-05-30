a20f96cc80a7311390557677e1fa96a51fa34cf6
Lucas JOLY <lucas.joly.0903@gmail.com>
2024-05-30 12:09:25 +0200

    reformates

--- src/security/Router.js
+++ src/security/Router.js
@@ -1,30 +1,35 @@
-import { lazy } from 'react';
-import { Navigate } from 'react-router-dom';
-import SecuredRoute from '../security/SecuredRoute.js';
-const FullLayout = lazy(() => import('../layouts/FullLayout.js'));
-const Home = lazy(() => import('../views/home/Home.js'));
-const SignIn = lazy(() => import('../views/signin/SingIn.js'));
-const AdminConsole = lazy(() => import('../views/admin/AdminConsole.js'));
+import { lazy } from "react";
+import { Navigate } from "react-router-dom";
+import SecuredRoute from "../security/SecuredRoute.js";
+const FullLayout = lazy(() => import("../layouts/FullLayout.js"));
+const Home = lazy(() => import("../views/home/Home.js"));
+const SignIn = lazy(() => import("../views/signin/SingIn.js"));
+const AdminConsole = lazy(() => import("../views/admin/AdminConsole.js"));
 const AdminGestion = lazy(() => import("../views/admin-gestion/AdminGestion"));
-const Questions = lazy(() => import('../views/questions/Questions.js'));
-const Summary = lazy(() => import('../views/summary/Summary.jsx'));
+const Questions = lazy(() => import("../views/questions/Questions.js"));
+const Summary = lazy(() => import("../views/summary/Summary.jsx"));
 
 const ThemeRoutes = [
-    {
-        path: "/",
-        element: <FullLayout/>,
-        children: [
-            {path: "/", element: <Navigate to="/accueil"/>},
-            {path: "/connection", exact: true, element: <SignIn/>},
-            {path: "/accueil", exact: true, element: <Home/>},
-            {path: "/questions/:id", exact: true, element: <Questions/>},
-            {path: "/summary/:id", exact: true, element: <Summary/>},
-            {
-                path: "/admin-console",
-                exact: true,
-                element: (<SecuredRoute childrenName="admin-console" children={<AdminConsole/>}></SecuredRoute>),
-            },
-        {
+  {
+    path: "/",
+    element: <FullLayout />,
+    children: [
+      { path: "/", element: <Navigate to="/accueil" /> },
+      { path: "/connection", exact: true, element: <SignIn /> },
+      { path: "/accueil", exact: true, element: <Home /> },
+      { path: "/questions/:id", exact: true, element: <Questions /> },
+      { path: "/summary/:id", exact: true, element: <Summary /> },
+      {
+        path: "/admin-console",
+        exact: true,
+        element: (
+          <SecuredRoute
+            childrenName="admin-console"
+            children={<AdminConsole />}
+          ></SecuredRoute>
+        ),
+      },
+      {
         path: "/admin-gestion",
         exact: true,
         element: (
@@ -34,13 +39,13 @@ const ThemeRoutes = [
           ></SecuredRoute>
         ),
       },
-            {
-                path: "/admin-consoletest",
-                exact: true,
-                element: <AdminConsole/>,
-            },
-        ],
-    },
+      {
+        path: "/admin-consoletest",
+        exact: true,
+        element: <AdminConsole />,
+      },
+    ],
+  },
 ];
 
 export default ThemeRoutes;
