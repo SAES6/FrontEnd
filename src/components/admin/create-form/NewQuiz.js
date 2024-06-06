2eb22c0c0446fe074324a0db884422c796a649f5
David-GINI <david.gini@etu.univ-amu.fr>
2024-06-06 11:53:00 +0200

    Final main layout changes for AdminConsole

--- src/components/admin/create-form/NewQuiz.js
+++ src/components/admin/create-form/NewQuiz.js
@@ -1,17 +1,24 @@
-import { Card, FormControl, Grid, TextField, Typography } from "@mui/material";
+import {
+  Card,
+  FormControl,
+  Grid,
+  Stack,
+  TextField,
+  Typography,
+} from '@mui/material';
 
 const FormattedTextField = ({ handleChange, placeholder, value, ...props }) => {
   return (
     <Grid
       item
       {...props}
-      sx={{ display: "flex", justifyContent: "flex-end", pl: 3, pr: 3 }}
+      sx={{ display: 'flex', justifyContent: 'flex-end', pl: 3, pr: 3 }}
     >
       <FormControl
         sx={{
-          display: "flex",
-          alignItems: "center",
-          width: "100%",
+          display: 'flex',
+          alignItems: 'center',
+          width: '100%',
         }}
       >
         <TextField
@@ -28,35 +35,35 @@ const FormattedTextField = ({ handleChange, placeholder, value, ...props }) => {
 
 const NewQuiz = ({ setQuizInfos, quizInfos }) => {
   return (
-    <Card sx={{ padding: 2, margin: 2, borderRadius: "15px" }}>
+    <Stack sx={{ width: '100%' }}>
       <Grid
         container
-        alignItems="center"
-        justifyContent="space-between"
+        alignItems='center'
+        justifyContent='space-between'
         spacing={2}
       >
         <Grid
           item
           xs={12}
           sx={{
-            display: "flex",
-            alignItems: "center",
+            display: 'flex',
+            alignItems: 'center',
           }}
         >
-          <Typography variant="h5" sx={{ fontWeight: "bold", pl: 2, mb: 2 }}>
+          <Typography variant='h5' sx={{ fontWeight: 'bold', pl: 2, mb: 2 }}>
             Informations générales
           </Typography>
         </Grid>
         <Grid
           item
           xs={6}
-          sx={{ display: "flex", justifyContent: "flex-end", pl: 3, pr: 3 }}
+          sx={{ display: 'flex', justifyContent: 'flex-end', pl: 3, pr: 3 }}
         >
           <FormControl
             sx={{
-              display: "flex",
-              alignItems: "center",
-              width: "100%",
+              display: 'flex',
+              alignItems: 'center',
+              width: '100%',
             }}
           >
             <TextField
@@ -64,7 +71,7 @@ const NewQuiz = ({ setQuizInfos, quizInfos }) => {
               onChange={(e) => setQuizInfos(e.target.value, null, null)}
               fullWidth
               required
-              placeholder="Nom du questionnaire"
+              placeholder='Nom du questionnaire'
             />
           </FormControl>
         </Grid>
@@ -72,27 +79,27 @@ const NewQuiz = ({ setQuizInfos, quizInfos }) => {
         <Grid
           item
           xs={4}
-          sx={{ display: "flex", justifyContent: "flex-end", pl: 3, pr: 3 }}
+          sx={{ display: 'flex', justifyContent: 'flex-end', pl: 3, pr: 3 }}
         >
           <Typography>Durée moyenne (min) :</Typography>
         </Grid>
         <Grid
           item
           xs={2}
-          sx={{ display: "flex", justifyContent: "flex-end", pl: 3, pr: 3 }}
+          sx={{ display: 'flex', justifyContent: 'flex-end', pl: 3, pr: 3 }}
         >
           <FormControl
             sx={{
-              display: "flex",
-              alignItems: "center",
-              width: "100%",
+              display: 'flex',
+              alignItems: 'center',
+              width: '100%',
             }}
           >
             <TextField
               value={quizInfos.duree}
               onChange={(e) => setQuizInfos(null, e.target.value, null)}
               fullWidth
-              type="number"
+              type='number'
               inputProps={{ min: 0, max: 60 }}
               required
             />
@@ -102,13 +109,13 @@ const NewQuiz = ({ setQuizInfos, quizInfos }) => {
         <Grid
           item
           xs={12}
-          sx={{ display: "flex", justifyContent: "flex-end", pl: 3, pr: 3 }}
+          sx={{ display: 'flex', justifyContent: 'flex-end', pl: 3, pr: 3 }}
         >
           <FormControl
             sx={{
-              display: "flex",
-              alignItems: "center",
-              width: "100%",
+              display: 'flex',
+              alignItems: 'center',
+              width: '100%',
             }}
           >
             <TextField
@@ -116,14 +123,14 @@ const NewQuiz = ({ setQuizInfos, quizInfos }) => {
               onChange={(e) => setQuizInfos(null, null, e.target.value)}
               fullWidth
               required
-              placeholder="Description du questionnaire"
-              size="small"
+              placeholder='Description du questionnaire'
+              size='small'
               multiline
             />
           </FormControl>
         </Grid>
       </Grid>
-    </Card>
+    </Stack>
   );
 };
 
