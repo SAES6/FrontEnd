c7334aa38a4481acbed9f304785a85033c2ec721
Jeremy <jeremy.gross@etu.univ-amu.fr>
2024-06-06 16:10:23 +0200

    j'ai réussi a merge

--- src/components/question/QuestionEchelle.jsx
+++ src/components/question/QuestionEchelle.jsx
@@ -1,15 +1,16 @@
-import { Grid, Typography, Slider } from "@mui/material";
-import { useTheme } from "@mui/material/styles";
-import { useEffect, useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { theme } from "../../theme";
-import Enonce from "./Enonce";
+import { Grid, Typography, Slider } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+import { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { theme } from '../../theme';
+import Enonce from './Enonce';
 
 const QuestionEchelle = ({
   onResponseChange,
   question,
   mode,
   userResponse = 0,
+  stats,
   preview,
 }) => {
   const [sliderValue, setSliderValue] = useState(question.slider_max / 2);
@@ -17,7 +18,7 @@ const QuestionEchelle = ({
   const themeQuestion = useTheme(theme);
 
   useEffect(() => {
-    if (mode === "question") onResponseChange(question.slider_max / 2);
+    if (mode === 'question') onResponseChange(question.slider_max / 2);
   }, []);
 
   const handleSliderChange = (event, newValue) => {
@@ -41,35 +42,35 @@ const QuestionEchelle = ({
 
   return (
     <Grid
-      className="question"
+      className='question'
       container
       sx={{
-        width: "100%",
-        height: "auto",
-        alignItems: "center",
-        justifyContent: "center",
-        alignContent: "center",
-        gap: "10px",
-        padding: "10px 0",
+        width: '100%',
+        height: 'auto',
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignContent: 'center',
+        gap: '10px',
+        padding: '10px 0',
       }}
     >
       <Grid
-        className="first-row"
+        className='first-row'
         sx={{
-          width: "100%",
-          height: "56px",
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
+          width: '100%',
+          height: '56px',
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
         }}
       >
         <Typography
           sx={{
-            fontFamily: "Poppins, sans-serif",
-            fontSize: "24px",
-            fontWeight: "600",
-            lineHeight: "36px",
+            fontFamily: 'Poppins, sans-serif',
+            fontSize: '24px',
+            fontWeight: '600',
+            lineHeight: '36px',
             color: themeQuestion.palette.text.primary,
           }}
         >
@@ -77,22 +78,22 @@ const QuestionEchelle = ({
         </Typography>
         <Grid
           sx={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
           }}
         >
           <FontAwesomeIcon
-            icon="fa-solid fa-sliders"
-            style={{ opacity: "0.5" }}
+            icon='fa-solid fa-sliders'
+            style={{ opacity: '0.5' }}
           />
           <Typography
             sx={{
-              fontFamily: "Poppins, sans-serif",
-              fontSize: "16px",
-              fontWeight: "600",
-              lineHeight: "24px",
-              marginLeft: "5px",
+              fontFamily: 'Poppins, sans-serif',
+              fontSize: '16px',
+              fontWeight: '600',
+              lineHeight: '24px',
+              marginLeft: '5px',
               color: themeQuestion.palette.text.primary,
             }}
           >
@@ -101,17 +102,17 @@ const QuestionEchelle = ({
         </Grid>
       </Grid>
       <Enonce description={question.description} imgSrc={question.img_src} />
-      {mode === "question" ? (
+      {mode === 'question' ? (
         <Grid
-          className="slider"
+          className='slider'
           sx={{
-            width: "100%",
+            width: '100%',
           }}
         >
           <Slider
             disabled={preview}
-            aria-label="Temperature"
-            valueLabelDisplay="auto"
+            aria-label='Temperature'
+            valueLabelDisplay='auto'
             value={sliderValue}
             onChange={handleSliderChange}
             step={question.slider_gap}
@@ -122,91 +123,91 @@ const QuestionEchelle = ({
         </Grid>
       ) : (
         <Grid
-          className="slider"
+          className='slider'
           sx={{
-            width: "100%",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            gap: "20px",
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            gap: '20px',
           }}
         >
           <Slider
-            aria-label="Temperature"
-            valueLabelDisplay="auto"
+            aria-label='Temperature'
+            valueLabelDisplay='auto'
             value={userResponse}
             onChange={handleSliderChange}
             step={question.slider_gap}
             marks={marks}
-            min={question.slider_mim}
+            min={question.slider_min}
             max={question.slider_max}
             sx={{
-              "&.MuiSlider-root": {
-                pointerEvents: "none !important",
+              '&.MuiSlider-root': {
+                pointerEvents: 'none !important',
               },
-              "&.MuiSlider-thumb": {
-                pointerEvents: "none !important",
+              '&.MuiSlider-thumb': {
+                pointerEvents: 'none !important',
               },
             }}
           />
           <Grid
             sx={{
-              width: "fit-content",
-              padding: "10px 15px",
-              border: "solid 1px",
+              width: 'fit-content',
+              padding: '10px 15px',
+              border: 'solid 1px',
               borderColor: themeQuestion.palette.secondary.main,
-              gap: "5px",
-              borderRadius: "15px",
+              gap: '5px',
+              borderRadius: '15px',
             }}
           >
             <Grid
               sx={{
-                display: "flex",
-                flexDirection: "row",
+                display: 'flex',
+                flexDirection: 'row',
               }}
             >
               <FontAwesomeIcon
-                icon="fa-solid fa-user"
+                icon='fa-solid fa-user'
                 style={{
-                  fontSize: "16px",
+                  fontSize: '16px',
                   color: themeQuestion.palette.primary.main,
                 }}
               />
               <Typography
                 sx={{
-                  marginLeft: "5px",
-                  fontFamily: "Poppins, sans-serif",
-                  fontSize: "16px",
-                  fontWeight: "400",
+                  marginLeft: '5px',
+                  fontFamily: 'Poppins, sans-serif',
+                  fontSize: '16px',
+                  fontWeight: '400',
                   color: themeQuestion.palette.primary.main,
                 }}
               >
-                6.1 moyenne des utilisateurs
+                {stats?.others?.mean ?? 'N/A'} moyenne des utilisateurs
               </Typography>
             </Grid>
             <Grid
               sx={{
-                display: "flex",
-                flexDirection: "row",
+                display: 'flex',
+                flexDirection: 'row',
               }}
             >
               <FontAwesomeIcon
-                icon="fa-solid fa-user-secret"
+                icon='fa-solid fa-user-secret'
                 style={{
-                  fontSize: "16px",
+                  fontSize: '16px',
                   color: themeQuestion.palette.secondary.main,
                 }}
               />
               <Typography
                 sx={{
-                  marginLeft: "5px",
-                  fontFamily: "Poppins, sans-serif",
-                  fontSize: "16px",
-                  fontWeight: "400",
+                  marginLeft: '5px',
+                  fontFamily: 'Poppins, sans-serif',
+                  fontSize: '16px',
+                  fontWeight: '400',
                   color: themeQuestion.palette.secondary.main,
                 }}
               >
-                5.7 moyenne des journalistes
+                {stats?.journalists?.mean ?? 'N/A'} moyenne des journalistes
               </Typography>
             </Grid>
           </Grid>
